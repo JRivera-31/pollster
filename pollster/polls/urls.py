a9e9e9f14398e60c6c6fa2614c46bf5c6d3fe611
Josh R <jriver35@uncc.edu>
2020-08-20 01:03:06 -0400

    created results path

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -7,5 +7,6 @@ app_name = 'polls'
 # basically creating routes
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>', views.detail, name='detail')
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results', views.results, name='results')
 ]
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -9,7 +9,7 @@ def index(request):
 
     # create object to pass to render method to access question data on front end
     context = {'latest_question_list': latest_question_list}
-    
+
     return render(request, 'polls/index.html', context)
 
 # Show specific questions and choices
@@ -23,4 +23,11 @@ def detail(request, question_id):
         raise Http404("Question does not exist")
     
     # return the question if try is successful
+    return render(request, 'polls/results.html', {'question': question})
+
+# Get question and display results
+def results(request, question_id):
+    # get the question
+    question = get_object_or_404(Question, pk=question_id)
+    # return the data
     return render(request, 'polls/results.html', {'question': question})
