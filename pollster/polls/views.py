255892a675b548e756992d6d81bdd16cb1a84768
Josh R <jriver35@uncc.edu>
2020-08-20 01:38:21 -0400

    created vote path

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Question, Choice
 
