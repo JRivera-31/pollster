b54d35a148d7b9272dae5184b50ea23cc9b64626
Josh R <jriver35@uncc.edu>
2020-08-20 00:57:50 -0400

    created detail path

--- pollster/polls/urls.py
+++ pollster/polls/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 app_name = 'polls'
-# basically creating a route
-urlpatterns = [path('', views.index, name='index')]
+# basically creating routes
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>', views.detail, name='detail')
+]
--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -4,4 +4,23 @@ from .models import Question, Choice
 
 # Get questions and display them
 def index(request):
-    return render(request, 'polls/index.html')
+    # create latest question list, order by ascending date and limit to 5
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # create object to pass to render method to access question data on front end
+    context = {'latest_question_list': latest_question_list}
+    
+    return render(request, 'polls/index.html', context)
+
+# Show specific questions and choices
+def detail(request, question_id):
+    # try to get the question by id
+    try:
+        question = Question.objects.get(pk=question_id)
+    
+    # send back error if question is not found
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    
+    # return the question if try is successful
+    return render(request, 'polls/results.html', {'question': question})
